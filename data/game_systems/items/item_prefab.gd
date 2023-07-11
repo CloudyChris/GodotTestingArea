51605babb971a8a3cec08af051850d7645e7ae84
CloudyChris <damianfate@gmail.com>
2023-07-12 01:03:37 +0300

    Added mouse capture on item_prefab, might change later to an interaction component

--- data/game_systems/items/item_prefab.gd
+++ data/game_systems/items/item_prefab.gd
@@ -1,8 +1,16 @@
 class_name ItemPrefab
-extends Node
+extends PhysicsBody3D
 
 @export var item_id : UUID
 
 func pick_up() -> UUID:
 	queue_free()
 	return item_id
+
+func _mouse_enter():
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+		print("entered")
+
+func _mouse_exit():
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_CAPTURED:
+		print("exited")
