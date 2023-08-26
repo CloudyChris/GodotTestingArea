135a1112acaa2804d85416d64408d55308f8ff59
CloudyChris <damianfate@gmail.com>
2023-08-26 23:53:03 +0300

    Added empty game manager, updated inventory resource with updates and overrides

--- data/game_systems/items/inventory.gd
+++ data/game_systems/items/inventory.gd
@@ -9,3 +9,17 @@ extends Resource
 		
 @export var inventory_id : UUID
 @export var items : Array[PackedItem] = []
+
+func update_from_inventory(anInventory:Inventory):
+	items = anInventory.items
+
+func overwrite_from_inventory(anInventory:Inventory):
+	inventory_id = anInventory.inventory_id
+	items = anInventory.items
+
+func update_from_array(aPackedItemArray:Array[PackedItem]):
+	items = aPackedItemArray
+
+func overwrite_manually(aUUID:UUID, aPackedItemArray:Array[PackedItem]):
+	inventory_id = aUUID
+	items = aPackedItemArray
